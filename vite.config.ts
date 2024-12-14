f8d04166def62ba9db9cbc23590d79fa08d8078d
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2024-12-14 17:32:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/helm-values-generator/'
+  base: '/helm-values-generator/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
